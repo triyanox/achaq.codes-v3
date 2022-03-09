debbc1193adbfa9140e6f10cc02420e6891f2cd6
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-09 20:34:14 +0100

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -7,7 +7,7 @@ const Intro = () => {
     <section className=" w-full h-screen flex flex-col-reverse lg:flex-row justify-center items-center px-2 md:px-16 xl:px-12 gap-y-8">
       <div className="text-black dark:text-white text-xl md:text-3xl xl:text-4xl flex flex-col justify-center items-center lg:items-start gap-y-2 w-full">
         <h1>Hey ! My name is Mohamed Achaq.</h1>
-        <h1>I'm a Full Stack Developer, </h1>
+        <h1>I&apos;m a Full Stack Developer, </h1>
         <h1>UI and UX designer and Artist.</h1>
         <Link href="/contact" passHref>
           <a className="mt-6 bg-gradient-to-r from-[#FF6B6B] to-[#6320EE] text-transparent bg-clip-text animate-gradient-x hover:-skew-y-3  transition duration-200 hover:scale-110 active:scale-90 hover:translate-x-5">

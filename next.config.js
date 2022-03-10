5551ad3aba0a61e4fecbffc1ccb0a5f963753448
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-10 18:06:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const nextConfig = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      });
-    }
-
-    return config;
   }
 };
 

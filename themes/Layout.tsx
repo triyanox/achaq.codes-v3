1fc17497cdaa39d6bcc26b7da78faede79351cda
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-10 15:31:09 +0100

    Update Layout.tsx

--- themes/Layout.tsx
+++ themes/Layout.tsx
@@ -91,7 +91,7 @@ const Layout = (props: {
         />
         <link rel="manifest" href="/manifest.json" />
        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+       <link rel="preconnect" href="https://fonts.gstatic.com" />
        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"/>
       </Head>
 

4d264087a9c70f91c1a71d247a840e3b2c6a11be
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-10 15:31:27 +0100

    Update BlogLayout.tsx

--- themes/BlogLayout.tsx
+++ themes/BlogLayout.tsx
@@ -82,7 +82,7 @@ const BlogLayout = (props: Props) => {
         />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+       <link rel="preconnect" href="https://fonts.gstatic.com" />
        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"/>
         
         <title>{props.pageTitle}</title>

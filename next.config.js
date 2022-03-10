5860169aa8c42415ad79826004e3265c5a9de327
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-10 18:26:51 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,17 +6,6 @@ const nextConfig = {
     locales: ["en"],
     defaultLocale: "en",
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;

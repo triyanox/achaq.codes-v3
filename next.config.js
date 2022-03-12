0a9d7e166e63011bf67dcde4262a47be299c9c5e
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-12 06:51:15 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,16 +9,6 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-   webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-    return config
-  },
 };
 
 module.exports = nextConfig;

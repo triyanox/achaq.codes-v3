2c0de6858685004edf45a23049c153deb86481b1
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-13 05:33:35 +0100

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import hero from "../assets/hero.svg";
+import hero from "../assets/hero-satatic.svg";
 import Link from "next/link";
 const Intro = () => {
   return (

28d64ec4a7f5e16a8f96f02607facbdfb91fa4a2
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-19 13:03:38 +0100

    Update BlogPrev.tsx

--- components/BlogPrev.tsx
+++ components/BlogPrev.tsx
@@ -7,17 +7,18 @@ interface Props {
 }
 const BlogPrev = (props: Props) => {
   return (
-    <div className="relative flex w-full h-40 md:h-56 justify-center flex-col active:scale-90 transition-all duration-300 ">
+    <div className="relative flex w-full h-40 md:h-56 justify-center flex-col hover:scale-110 active:scale-90 transition-all duration-300 ">
       <div className="flex flex-col gap-2 p-2">
+        
+        <h1 className="text-2xl md:text-3xl  tracking-tight text-[#6320EE] dark:text-[#FF6B6B] mb-2">
+          {props.title}
+        </h1>
         <div className="flex flex-row justify-between mb-2 ">
           <div className="text-xs text-black dark:text-white">{props.date}</div>
           <div className="text-xs text-black dark:text-white">
             {props.readTime + " min read"}
           </div>
         </div>
-        <h1 className="text-2xl md:text-3xl  tracking-tight text-[#6320EE] dark:text-[#FF6B6B] mb-2">
-          {props.title}
-        </h1>
         <h3 className="text-md md:text-lg text-black dark:text-white">
           {props.description}
         </h3>

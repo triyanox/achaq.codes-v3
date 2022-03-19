b246b7572d84bc3c0b5d9b83b9cb125aacae14b4
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-19 13:15:44 +0100

    Update BlogPrev.tsx

--- components/BlogPrev.tsx
+++ components/BlogPrev.tsx
@@ -10,7 +10,7 @@ const BlogPrev = (props: Props) => {
     <div className="relative flex w-full h-40 md:h-56 justify-center flex-col hover:border-2 border-[#6320EE] dark:border-[#FF6B6B] active:scale-90 transition-all duration-100 rounded-xl p-2">
       <div className="flex flex-col gap-2 p-2">
         
-        <h1 className="text-2xl md:text-3xl  tracking-tight text-[#6320EE] dark:text-[#FF6B6B] mb-2">
+        <h1 className="text-xl md:text-2xl  tracking-tight text-[#6320EE] dark:text-[#FF6B6B] mb-2">
           {props.title}
         </h1>
         <div className="flex flex-row justify-between mb-2 ">
@@ -19,7 +19,7 @@ const BlogPrev = (props: Props) => {
             {props.readTime + " min read"}
           </div>
         </div>
-        <h3 className="text-md md:text-lg text-black dark:text-white">
+        <h3 className="text-sm md:text-md text-black dark:text-white">
           {props.description}
         </h3>
       </div>

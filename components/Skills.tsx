7d0e95c62cf9350c7e37087b8ceabcd7c0d69ba9
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-21 13:52:08 +0100

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,7 +3,6 @@ import { MdDesignServices } from "react-icons/md";
 import { RiUser4Fill } from "react-icons/ri";
 import { BsFront } from "react-icons/bs";
 import { FiServer } from "react-icons/fi";
-import { Parallax } from "react-scroll-parallax";
 const Skills = () => {
   return (
     <section className="flex flex-col w-full justify-center px-8 md:px-16 xl:px-12 gap-y-8 sm:mt-16 lg:mt-0 ">
@@ -14,7 +13,6 @@ const Skills = () => {
         <h2 className="text-xl mt-4">I can provide </h2>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mb-4">
-        <Parallax speed={2}>
           <div className="grid grid-rows-3 justify-start items-center p-2 px-6 bg-gradient-to-br from-[#FF6B6B] to-[#8C271E] w-full rounded-xl h-64 lg:h-96 gap-y-2 ">
             <div className="flex flex-row gap-8">
               <p className="text-2xl md:text-4xl">
@@ -29,8 +27,6 @@ const Skills = () => {
               </h2>
             </div>
           </div>
-        </Parallax>
-        <Parallax speed={5}>
           <div className="grid grid-rows-3 justify-start items-center p-2 px-6 bg-gradient-to-br from-[#FFCDBC] to-[#FF6B6B] w-full rounded-xl h-64 lg:h-96 gap-y-2 ">
             <div className="flex flex-row gap-8">
               <p className="text-2xl md:text-4xl">
@@ -45,8 +41,6 @@ const Skills = () => {
               </h2>
             </div>
           </div>
-        </Parallax>
-        <Parallax speed={15}>
           <div className="grid grid-rows-3 justify-start items-center p-2 px-6 bg-gradient-to-br from-[#FBAF00] to-[#F2F79E] w-full rounded-xl h-64 lg:h-96 gap-y-2 ">
             <div className="flex flex-row gap-8">
               <p className="text-2xl md:text-4xl">
@@ -61,8 +55,6 @@ const Skills = () => {
               </h2>
             </div>
           </div>
-        </Parallax>
-        <Parallax speed={10}>
           <div className="grid grid-rows-3 justify-start items-center p-2 px-6 bg-gradient-to-br from-[#17BEBB] to-[#7F96FF] w-full rounded-xl h-64 lg:h-96 gap-y-2">
             <div className="flex flex-row gap-8">
               <p className="text-2xl md:text-4xl">
@@ -77,7 +69,6 @@ const Skills = () => {
               </h2>
             </div>
           </div>
-        </Parallax>
       </div>
     </section>
   );

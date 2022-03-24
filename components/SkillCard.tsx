533a6ea3e03bb8b45d118f5fad59041b0ff254e2
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-24 05:09:46 +0100

    Update SkillCard.tsx

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -9,8 +9,8 @@ interface Props {
 
 const SkillCard = (props: Props) => {
   return (
-    <div className="flex justify-center items-center p-1 w-full rounded-xl h-40 gap-4">
-      <div className="grid grid-rows-3 justify-center items-center p-4 bg-white dark:bg-black text-black dark:text-white rounded-xl w-full h-full gap-2">
+    <div className="flex justify-start items-center p-1 w-full rounded-xl h-40 gap-4">
+      <div className="grid grid-rows-3 justify-start items-center p-4 bg-white dark:bg-black text-black dark:text-white rounded-xl w-full h-full gap-2">
         <div className="flex flex-row gap-2">
           <p className="text-xl md:text-3xl">{props.icon}</p>
           <h1 className="text-xl lg:text-2xl">{props.title}</h1>

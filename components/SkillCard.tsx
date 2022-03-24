24363e3bd586c0cd6069c10848e85b53a6649189
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-24 16:32:31 +0100

    Gradient border

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -9,13 +9,15 @@ interface Props {
 
 const SkillCard = (props: Props) => {
   return (
-    <div className="flex justify-start items-center p-1 w-full rounded-xl h-40 gap-4">
-      <div className="grid grid-rows-3 justify-start items-center p-4 bg-white dark:bg-black text-black dark:text-white rounded-xl w-full h-full gap-2">
+    <div className="flex justify-start items-center p-1 w-full rounded-2xl h-40 gap-4 hover:bg-gradient-to-r from-[#6E44FF]  to-[#6EE7B7] dark:from-[#EB5160] dark:to-[#3B82F6] animate-gradient-x transition-all duration-500">
+      <div className="grid grid-rows-3 justify-start items-center p-4 bg-white dark:bg-black text-black dark:text-white rounded-2xl w-full h-full gap-2">
         <div className="flex flex-row gap-2">
           <p className="text-xl md:text-3xl">{props.icon}</p>
           <h1 className="text-xl lg:text-2xl">{props.title}</h1>
         </div>
-        <h2 className="text-md mt-2 text-gray-700 dark:text-gray-200">{props.description}</h2>
+        <h2 className="text-md mt-2 text-gray-700 dark:text-gray-200">
+          {props.description}
+        </h2>
       </div>
     </div>
   );

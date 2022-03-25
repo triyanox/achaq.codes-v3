f01ecb730d45a0202869e6543d7eed8d8c189e6d
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-25 20:55:27 +0100

    Update CodeBlock.tsx

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BsCheck2Circle } from "react-icons/bs";
 import { HiClipboardCopy } from "react-icons/hi";
 interface CodeBlockProps {
@@ -8,7 +8,9 @@ interface CodeBlockProps {
 export default function CodeBlock({ children }: CodeBlockProps) {
   const preRef = useRef<HTMLPreElement>(null);
   const [isCopied, setIsCopied] = useState<boolean>(false);
-
+  useEffect(() => {
+    setIsCopied(false);
+  }, []);
   function copy() {
     const content = preRef.current?.textContent ?? "";
     navigator.clipboard.writeText(content);

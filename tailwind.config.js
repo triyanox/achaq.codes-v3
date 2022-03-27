17d45e7d41c95ebe46a7168783e46841e0bfe8a7
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-27 22:57:16 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
        fontFamily: {
-        sans: ['Dosis', ...defaultTheme.fontFamily.sans],
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
       animation: {
         "gradient-x": "gradient-x 3s ease infinite",

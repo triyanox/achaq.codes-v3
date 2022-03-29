058f811d8ccd93269f8d6d02bba46401ddc52a27
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-03-29 13:31:26 +0000

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,8 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>  </Head>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>      
+      </Head>
       <body className="bg-white dark:bg-black antialiased">
         <Main />
         <NextScript />

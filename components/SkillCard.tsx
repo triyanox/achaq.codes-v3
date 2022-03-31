3fce1b08991261d7d1c387a745d58167b7e22dd2
Mohamed Achaq <achaq@n.local>
2022-03-31 04:41:15 +0000

    Background Blur with Gradient

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { IconType } from "react-icons/lib";
 
 interface Props {
   icon: ReactNode;
@@ -9,11 +8,11 @@ interface Props {
 
 const SkillCard = (props: Props) => {
   return (
-    <div className="flex justify-start items-center w-full rounded-xl h-40 gap-4 hover:bg-gradient-to-r from-[#6E44FF]  to-[#6EE7B7] dark:from-[#EB5160] dark:to-[#3B82F6] animate-gradient-x hover:shadow-xl dark:shadow-[#EB5160]">
-      <div className="grid grid-rows-3 justify-start items-center p-4 bg-white dark:bg-black rounded-xl w-full h-full gap-2 bg-opacity-70 dark:bg-opacity-70 backdrop-filter backdrop-blur-xl dark:backdrop-blur-xl">
-        <div className="flex flex-row gap-2">
+    <div className="flex justify-start items-center w-full rounded-xl h-40 gap-4 hover:bg-gradient-to-r from-[#6E44FF]  to-[#6EE7B7] dark:from-[#EB5160] dark:to-[#3B82F6] animate-gradient-x hover:shadow-xl dark:shadow-[#EB5160] transition-all duration-300 ease-in-out select-none">
+      <div className="grid grid-rows-3 justify-start items-center p-4 bg-white dark:bg-black rounded-xl w-full h-full gap-2 bg-opacity-70 dark:bg-opacity-70 backdrop-filter backdrop-blur-3xl dark:backdrop-blur-3xl transition-all duration-300 ease-in-out">
+        <div className="flex flex-row gap-6">
           <p className="text-xl md:text-3xl">{props.icon}</p>
-          <h1 className="text-xl lg:text-2xl">{props.title}</h1>
+          <h1 className="text-xl lg:text-2xl font-medium">{props.title}</h1>
         </div>
         <h2 className="text-md mt-2 text-gray-700 dark:text-gray-200">
           {props.description}

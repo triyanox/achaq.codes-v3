215130cbbef4e7e8dc145478d456dd2b101cd065
Mohamed Achaq <achaq@n.local>
2022-03-31 04:39:42 +0000

    Background Blur with Gradient

--- components/BlogPrev.tsx
+++ components/BlogPrev.tsx
@@ -7,20 +7,24 @@ interface Props {
 }
 const BlogPrev = (props: Props) => {
   return (
-    <div className="relative flex w-full h-40 md:h-56 justify-center flex-col  active:scale-90 p-2 hover:bg-gray-200 dark:hover:bg-zinc-800 rounded-lg shadow-sm  hover:scale-110 transition-all duration-300">
-      <div className="flex flex-col gap-2 p-2">
-        <h1 className="text-xl md:text-2xl text-black dark:text-white font-semibold mb-2">
-          {props.title}
-        </h1>
-        <div className="flex flex-row justify-between mb-2 ">
-          <div className="text-sm text-black dark:text-white">{props.date}</div>
-          <div className="text-sm text-black dark:text-white">
-            {props.readTime + " min read"}
+    <div className="flex justify-start items-center md:h-56 active:scale-90 rounded-xl w-full h-48 gap-4 hover:bg-gradient-to-r from-[#6E44FF]  to-[#6EE7B7] dark:from-[#EB5160] dark:to-[#3B82F6] animate-gradient-x hover:shadow-2xl dark:shadow-[#EB5160] transition-all duration-300 ease-in-out select-none">
+      <div className="relative flex justify-center flex-col  p-2 bg-white dark:bg-black rounded-xl w-full h-full gap-2 bg-opacity-70 dark:bg-opacity-70 backdrop-filter backdrop-blur-3xl dark:backdrop-blur-3xl transition-all duration-300 ease-in-out">
+        <div className="flex flex-col gap-2 p-2">
+          <h1 className="text-lg md:text-2xl text-black dark:text-white font-medium mb-2">
+            {props.title}
+          </h1>
+          <div className="flex flex-row justify-between mb-2 ">
+            <div className="text-xs text-black dark:text-white">
+              {props.date}
+            </div>
+            <div className="text-xs text-black dark:text-white">
+              {props.readTime + " min read"}
+            </div>
           </div>
+          <h3 className="text-sm md:text-md text-black dark:text-white">
+            {props.description}
+          </h3>
         </div>
-        <h3 className="text-sm md:text-md text-black dark:text-white">
-          {props.description}
-        </h3>
       </div>
     </div>
   );

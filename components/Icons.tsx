8787cfd8edd293d47780142900391da4b9a7fbc9
Mohamed Achaq <achaq@n.local>
2022-03-31 04:40:52 +0000

    Icons With Hover States

---
+++ components/Icons.tsx
@@ -0,0 +1,164 @@
+import {
+  SiFirebase,
+  SiAdobephotoshop,
+  SiAdobexd,
+  SiGit,
+  SiGraphql,
+  SiTensorflow,
+  SiMongodb,
+  SiExpress,
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiTypescript,
+  SiPython,
+  SiNodedotjs,
+  SiReact,
+  SiNextdotjs,
+  SiSass,
+  SiTailwindcss,
+} from "react-icons/si";
+
+export const Html5 = () => {
+  return (
+    <div className="hover:text-orange-600 hover:scale-125 transition-all duration-150">
+      <SiHtml5 />
+    </div>
+  );
+};
+
+export const Css3 = () => {
+  return (
+    <div className="hover:text-blue-600 hover:scale-125 transition-all duration-150">
+      <SiCss3 />
+    </div>
+  );
+};
+
+export const Javascript = () => {
+  return (
+    <div className="hover:text-yellow-500 hover:scale-125 transition-all duration-150">
+      <SiJavascript />
+    </div>
+  );
+};
+
+export const Typescript = () => {
+  return (
+    <div className="hover:text-blue-800 hover:scale-125 transition-all duration-150">
+      <SiTypescript />
+    </div>
+  );
+};
+
+export const Python = () => {
+  return (
+    <div className="hover:text-blue-700 hover:scale-125 transition-all duration-150">
+      <SiPython />
+    </div>
+  );
+};
+
+export const Nodedotjs = () => {
+  return (
+    <div className="hover:text-green-600 hover:scale-125 transition-all duration-150">
+      <SiNodedotjs />
+    </div>
+  );
+};
+
+export const Reactdotjs = () => {
+  return (
+    <div className="hover:text-cyan-600 hover:scale-125 transition-all duration-150">
+      <SiReact />
+    </div>
+  );
+};
+
+export const Nextdotjs = () => {
+  return (
+    <div className="hover:text-gray-700 hover:scale-125 transition-all duration-150">
+      <SiNextdotjs />
+    </div>
+  );
+};
+
+export const Sass = () => {
+  return (
+    <div className="hover:text-pink-600 hover:scale-125 transition-all duration-150">
+      <SiSass />
+    </div>
+  );
+};
+
+export const Tailwindcss = () => {
+  return (
+    <div className="hover:text-blue-500 hover:scale-125 transition-all duration-150">
+      <SiTailwindcss />
+    </div>
+  );
+};
+
+export const Mongodb = () => {
+  return (
+    <div className="hover:text-green-600 hover:scale-125 transition-all duration-150">
+      <SiMongodb />
+    </div>
+  );
+};
+
+export const Express = () => {
+  return (
+    <div className="hover:text-gray-600 hover:scale-125 transition-all duration-150">
+      <SiExpress />
+    </div>
+  );
+};
+
+export const Graphql = () => {
+  return (
+    <div className="hover:text-rose-600 hover:scale-125 transition-all duration-150">
+      <SiGraphql />
+    </div>
+  );
+};
+
+export const Tensorflow = () => {
+  return (
+    <div className="hover:text-amber-600 hover:scale-125 transition-all duration-150">
+      <SiTensorflow />
+    </div>
+  );
+};
+
+export const Git = () => {
+  return (
+    <div className="hover:text-red-700 hover:scale-125 transition-all duration-150">
+      <SiGit />
+    </div>
+  );
+};
+
+export const Firebase = () => {
+  return (
+    <div className="hover:text-amber-400 hover:scale-125 transition-all duration-150">
+      <SiFirebase />
+    </div>
+  );
+};
+
+export const Adobexd = () => {
+  return (
+    <div className="hover:text-rose-700 hover:scale-125 transition-all duration-150">
+      <SiAdobexd />
+    </div>
+  );
+};
+
+export const Adobephotoshop = () => {
+  return (
+    <div className="hover:text-blue-800 hover:scale-125 transition-all duration-150">
+      <SiAdobephotoshop />
+    </div>
+  );
+};

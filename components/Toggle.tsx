222b2a990a646367390990835d0a4f6269a1eb3d
Mohamed Achaq <achaq@n.local>
2022-03-31 04:41:30 +0000

    New Icons for Dark and Light Themes

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { motion } from "framer-motion";
 
+import { RiMoonClearFill } from "react-icons/ri";
+import { IoMdSunny } from "react-icons/io";
+
 const Toggle = () => {
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
@@ -14,39 +17,9 @@ const Toggle = () => {
         <button
           aria-label="Light Theme"
           onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-          className="text-black dark:text-white text-3xl cursor-pointer active:rotate-90 transition-all duration-500"
+          className="text-black dark:text-white text-xl cursor-pointer active:rotate-90 transition-all duration-500"
         >
-          {resolvedTheme === "dark" ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-              />
-            </svg>
-          )}
+          {resolvedTheme === "dark" ? <IoMdSunny /> : <RiMoonClearFill />}
         </button>
       )}
     </motion.div>

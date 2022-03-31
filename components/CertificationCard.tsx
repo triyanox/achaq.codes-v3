ff088905e724a6fc34c01efddadc9aa06f18637f
Mohamed Achaq <achaq@n.local>
2022-03-31 04:39:58 +0000

    Provider Field Added

--- components/CertificationCard.tsx
+++ components/CertificationCard.tsx
@@ -5,13 +5,14 @@ import { CgChevronRight } from "react-icons/cg/index.js";
 interface Props {
   title: string;
   link: string;
+  provider: string;
 }
 
 const CertificationCard = (props: Props) => {
   return (
     <Link href={props.link} passHref>
       <a target="_blank">
-        <div className="flex flex-col gap-1 items-start sm:items-center md:items-start justify-center px-2 md:px-6 py-1 w-full hover:bg-gray-200 dark:hover:bg-zinc-800 text-black dark:text-white hover:text-[#6E44FF] dark:hover:text-[#EB5160] rounded-lg hover:scale-110 transition-all duration-200">
+        <div className=" flex flex-col gap-1 items-start sm:items-center md:items-start justify-center px-2 md:px-6 py-1 w-full hover:bg-gray-200 dark:hover:bg-zinc-800 text-black dark:text-white hover:text-[#6E44FF] dark:hover:text-[#EB5160] rounded-lg hover:scale-110 transition-all duration-200">
           {" "}
           <div className="w-full flex flex-row gap-2 text-lg md:text-xl tracking-tight mb-1  justify-start items-center">
             <div>

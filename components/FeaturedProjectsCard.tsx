71e582ab852793401b36257c41b8716ae4237099
Mohamed Achaq <motriyanox@gmail.com>
2022-04-10 01:00:12 +0000

    Update FeaturedProjectsCard.tsx

--- components/FeaturedProjectsCard.tsx
+++ components/FeaturedProjectsCard.tsx
@@ -76,7 +76,7 @@ const FeaturedProjectsCard = (props: Props) => {
           </div>
           <div className="flex flex-row justify-start text-2xl md:text-3xl gap-3 text-gray-700 dark:text-gray-300 p-1">
             {props.stack.map((item, index) => (
-              <div key={index}>{item}</div>
+              <li key={index}>{item}</li>
             ))}
           </div>
         </div>

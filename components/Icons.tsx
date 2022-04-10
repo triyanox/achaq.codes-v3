c7feea0f5ddd71c34a07f345c256274cf83fa178
Mohamed Achaq <motriyanox@gmail.com>
2022-04-10 00:33:45 +0000

    Create new icons with tooltips

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -21,144 +21,288 @@ import {
 
 export const Html5 = () => {
   return (
-    <div className="hover:text-orange-600 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-orange-600 hover:scale-125 transition-all duration-150 group">
       <SiHtml5 />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-6 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        HTML 5
+      </span>
     </div>
   );
 };
 
 export const Css3 = () => {
   return (
-    <div className="hover:text-blue-600 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-blue-600 hover:scale-125 transition-all duration-150 group">
       <SiCss3 />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-3 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        CSS 3
+      </span>
     </div>
   );
 };
 
 export const Javascript = () => {
   return (
-    <div className="hover:text-yellow-500 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-yellow-500 hover:scale-125 transition-all duration-150 group">
       <SiJavascript />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-8 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Javascript
+      </span>
     </div>
   );
 };
 
 export const Typescript = () => {
   return (
-    <div className="hover:text-blue-800 hover:scale-125 transition-all duration-150">
+    <div className=" hover:text-blue-800 hover:scale-125 transition-all duration-150 group">
       <SiTypescript />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-8 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Typescript
+      </span>
     </div>
   );
 };
 
 export const Python = () => {
   return (
-    <div className="hover:text-blue-700 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-blue-700 hover:scale-125 transition-all duration-150 group">
       <SiPython />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-6 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Python
+      </span>
     </div>
   );
 };
 
 export const Nodedotjs = () => {
   return (
-    <div className="hover:text-green-600 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-green-600 hover:scale-125 transition-all duration-150 group">
       <SiNodedotjs />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-6 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Node.js
+      </span>
     </div>
   );
 };
 
 export const Reactdotjs = () => {
   return (
-    <div className="hover:text-cyan-600 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-cyan-600 hover:scale-125 transition-all duration-150 group">
       <SiReact />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-5 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        React
+      </span>
     </div>
   );
 };
 
 export const Nextdotjs = () => {
   return (
-    <div className="hover:text-gray-700 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-black dark:hover:text-white hover:scale-125 transition-all duration-150 group">
       <SiNextdotjs />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-4 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Next.js
+      </span>
     </div>
   );
 };
 
 export const Sass = () => {
   return (
-    <div className="hover:text-pink-600 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-pink-600 hover:scale-125 transition-all duration-150 group">
       <SiSass />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-2 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Sass
+      </span>
     </div>
   );
 };
 
 export const Tailwindcss = () => {
   return (
-    <div className="hover:text-blue-500 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-blue-500 hover:scale-125 transition-all duration-150 group">
       <SiTailwindcss />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-10 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Tailwind CSS
+      </span>
     </div>
   );
 };
 
 export const Mongodb = () => {
   return (
-    <div className="hover:text-green-600 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-green-600 hover:scale-125 transition-all duration-150 group">
       <SiMongodb />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-8 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        MongoDB
+      </span>
     </div>
   );
 };
 
 export const Express = () => {
   return (
-    <div className="hover:text-gray-600 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-black dark:hover:text-white hover:scale-125 transition-all duration-150 group">
       <SiExpress />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-6 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Express
+      </span>
     </div>
   );
 };
 
 export const Graphql = () => {
   return (
-    <div className="hover:text-rose-600 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-rose-600 hover:scale-125 transition-all duration-150 group">
       <SiGraphql />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-8 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        GraphQL
+      </span>
     </div>
   );
 };
 
 export const Tensorflow = () => {
   return (
-    <div className="hover:text-amber-600 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-amber-600 hover:scale-125 transition-all duration-150 group">
       <SiTensorflow />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-8 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Tensorflow
+      </span>
     </div>
   );
 };
 
 export const Git = () => {
   return (
-    <div className="hover:text-red-700 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-red-700 hover:scale-125 transition-all duration-150 group">
       <SiGit />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-3 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Git
+      </span>
     </div>
   );
 };
 
 export const Firebase = () => {
   return (
-    <div className="hover:text-amber-400 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-amber-400 hover:scale-125 transition-all duration-150 group">
       <SiFirebase />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-7 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Firebase
+      </span>
     </div>
   );
 };
 
 export const Adobexd = () => {
   return (
-    <div className="hover:text-rose-700 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-rose-700 hover:scale-125 transition-all duration-150 group">
       <SiAdobexd />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-6 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Adobe XD
+      </span>
     </div>
   );
 };
 
 export const Adobephotoshop = () => {
   return (
-    <div className="hover:text-blue-800 hover:scale-125 transition-all duration-150">
+    <div className="hover:text-blue-800 hover:scale-125 transition-all duration-150 group">
       <SiAdobephotoshop />
+      <span
+        className="absolute w-auto p-2 m-2 min-w-max -right-11 rounded-md shadow-md
+                    text-white bg-black  dark:bg-white dark:text-black 
+    text-xs font-bold 
+    transition-all duration-100 scale-0 origin-top group-hover:scale-100"
+      >
+        Adobe Photoshop
+      </span>
     </div>
   );
 };

0adeeedeb78bfe5c33598fc93ffd832bdb929275
Mohamed Achaq <motriyanox@gmail.com>
2022-04-10 01:19:01 +0000

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -89,7 +89,6 @@ function Projects() {
             <Tailwindcss />,
             <Typescript />,
             <Nodedotjs />,
-            <Javascript />,
             <Express />,
             <Mongodb />,
           ]}

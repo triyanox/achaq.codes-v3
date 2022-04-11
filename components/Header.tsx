43ecf383860e5d1cd2c8e21d90e7dac11b996611
Mohamed Achaq <motriyanox@gmail.com>
2022-04-11 00:01:38 +0000

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { useContext } from "react";
 import { ActiveContext } from "./ActiveContext";
 import Toggle from "./Toggle";
@@ -35,7 +34,7 @@ function NavItem({ href, text }: NavItems) {
 function Header() {
   const { active, setActive } = useContext(ActiveContext);
   return (
-    <section className="fixed flex top-0 h-16 items-center py-2 w-full bg-white dark:bg-black px-8 md:px-24 xl:px-20 z-50 bg-opacity-40 dark:bg-opacity-40 backdrop-filter backdrop-blur-xl">
+    <section className="fixed flex top-0 h-16 items-center py-2 w-full bg-zinc-50 dark:bg-zinc-900 px-8 md:px-24 xl:px-20 z-50 bg-opacity-40 dark:bg-opacity-40 backdrop-filter backdrop-blur-xl">
       <nav className=" w-full  flex items-center justify-between flex-row ">
         <div className=" w-full lg:inline-flex  hidden lg:visible text-xl ">
           <NavItem href="/" text="Home" />

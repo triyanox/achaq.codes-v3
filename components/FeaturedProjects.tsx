553458fe8f56a58f2d1d394348291c24feb87a96
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-04-12 05:50:44 +0000

    Update FeaturedProjects.tsx

--- components/FeaturedProjects.tsx
+++ components/FeaturedProjects.tsx
@@ -39,7 +39,6 @@ const FeaturedProjects = () => {
             <Reactdotjs />,
             <Tailwindcss />,
             <Typescript />,
-            <Reactdotjs />,
           ]}
         />
         <FeaturedProjectsCard

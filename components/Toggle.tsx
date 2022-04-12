7d5b192b238ee2def6d94760edf91c699614a127
Mohamed Achaq <motriyanox@gmail.com>
2022-04-12 23:32:40 +0000

    Update Toggle.tsx

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -3,7 +3,7 @@ import { useTheme } from "next-themes";
 import { motion } from "framer-motion";
 
 import { FaMoon } from "react-icons/fa";
-import { BsFillSunFill } from 'react-icons/bs'
+import { ImSun } from "react-icons/im";
 
 const Toggle = () => {
   const [mounted, setMounted] = useState(false);
@@ -19,7 +19,7 @@ const Toggle = () => {
           onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
           className="text-black dark:text-white text-xl cursor-pointer active:rotate-90 transition-all duration-500"
         >
-          {resolvedTheme === "dark" ? <BsFillSunFill /> : <FaMoon />}
+          {resolvedTheme === "dark" ? <ImSun /> : <FaMoon />}
         </button>
       )}
     </motion.div>
